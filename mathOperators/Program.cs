07b7a7b465e4cc7ed52726d4262ac99968fe5ec3
rabell66 <raabell66@gmail.com>
2017-08-13 09:56:45 -0500

    final

---
+++ mathOperators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace mathOperators
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            string x = Console.ReadLine();
+            decimal firstNum = Int32.Parse(x);
+            Console.WriteLine("Enter another Number");
+            string y = Console.ReadLine();
+            decimal secondNum = Int32.Parse(y);
+
+
+            decimal additionResults = firstNum + secondNum;
+            decimal subtractionResults = firstNum - secondNum;
+            decimal multiplicationResults = firstNum * secondNum;
+            decimal divisionResults = firstNum / secondNum;
+
+            decimal.Round(divisionResults, 2, MidpointRounding.AwayFromZero);
+            decimal modulusResults = firstNum % secondNum;
+
+            Console.WriteLine("The total of " + firstNum + " and " + secondNum + " is " + additionResults + "\n\n");
+            Console.WriteLine("The total of " + firstNum + " minus " + secondNum + " is " + subtractionResults + "\n\n");
+            Console.WriteLine("The total of " + firstNum + " multiplied by " + secondNum + " is " + multiplicationResults + "\n\n");
+            Console.WriteLine("The total of " + firstNum + " divided by " + secondNum + " is " + divisionResults + "\n\n");
+            Console.WriteLine("The modulus of " + firstNum + " and  " + secondNum + " is " + modulusResults + "\n\n");
+
+
+
+        }
+    }
+}
